d5489f83a4d4f4b00a2b76224ad711256d03f4ca
gagjunior <gagjunior@hotmail.com>
2022-03-29 23:44:36 -0300

    Criar menu

--- source.cpp
+++ source.cpp
@@ -1,5 +1,46 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int menu();
+
+struct Aluno
+{
+    char nomeAluno[61];
+    char emailAluno[61];
+    int ruAluno;
+};
+
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int opcao = 0;
+
+    while (opcao != 2)
+    {
+        opcao = menu();
+    }
+    
+    
     return 0;
+}
+
+int menu()
+{
+	int c, opcao;
+
+	// Titulo da aplicação
+	printf("\nBuscador de Aluno\n");
+	// Opções do menu
+	printf("1 - Buscar Aluno\n");
+	printf("2 - Sair\n\n");
+	
+	// Solicita a opção
+	printf("Digite o numero da opcao desejada: ");
+	scanf("%d", &opcao);
+	while ((c = getchar()) != '\n' && c != EOF)
+	{
+	}
+	printf("\n");
+
+	return opcao;
 }
